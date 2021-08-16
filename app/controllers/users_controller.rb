fea7c5488f67347444f5a726043a7be65a3bad06
Dan Beasley <dannyleebeasley@gmail.com>
2021-08-16 14:13:10 -0500

    commit tree 2c2d5da5605bfc07cb6ff1b0fee1d744bccc7630

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+    wrap_parameters format: []
+
+    def show
+        user = User.find(params[:id])
+        render json: user, except: [ :created_at, :updated_at ], include: [ items: { except: [ :created_at, :updated_at ] }]
+    end
+
 end
